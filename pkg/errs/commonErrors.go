7bffbd6a73404607a0c95e087a4a7c3d22a670d6
yuri <yohannesh.2018@gmail.com>
2025-01-08 10:10:36 +0300

    add some common error types

--- pkg/errs/commonErrors.go
+++ pkg/errs/commonErrors.go
@@ -2,6 +2,52 @@ package errs
 
 import "github.com/gin-gonic/gin"
 
+func BadRequest(c *gin.Context, messages ...string) {
+	error := CommonError{
+		StatusCode: 400,
+		Status:     "bad request",
+		Messages:   messages,
+	}
+	c.Error(&error)
+	return
+}
+func Unauthorized(c *gin.Context, messages ...string) {
+	error := CommonError{
+		StatusCode: 401,
+		Status:     "unauthorized",
+		Messages:   messages,
+	}
+	c.Error(&error)
+	return
+}
+func Forbidden(c *gin.Context, messages ...string) {
+	error := CommonError{
+		StatusCode: 403,
+		Status:     "forbidden",
+		Messages:   messages,
+	}
+	c.Error(&error)
+	return
+}
+func NotFound(c *gin.Context, messages ...string) {
+	error := CommonError{
+		StatusCode: 403,
+		Status:     "not found",
+		Messages:   messages,
+	}
+	c.Error(&error)
+	return
+}
+func Conflict(c *gin.Context, messages ...string) {
+	error := CommonError{
+		StatusCode: 409,
+		Status:     "conflict",
+		Messages:   messages,
+	}
+	c.Error(&error)
+	return
+}
+
 func InternalServerError(c *gin.Context, messages ...string) {
 	error := CommonError{
 		StatusCode: 500,
@@ -11,3 +57,4 @@ func InternalServerError(c *gin.Context, messages ...string) {
 	c.Error(&error)
 	return
 }
+
